3d571fcd589f8df89fd7ffa2b5a24be91ba39ed0
minji <ohora.ming@gmail.com>
2024-04-04 09:16:36 +0900

    cleanUp

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-
-  const onClick = () => setValue((prev) => prev + 1);
-  
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log("I run all the time"); // 매번 실행
-  
+function Hello() {
   useEffect(() => {
-    console.log("I run only once"); // 최초 한 번만 실행
+    console.log("I'm Here!");
   }, []);
-  
-  useEffect(() => {
-    console.log("I run when 'keyword' changes"); // keyword의 변화가 있을 때만 실행
-  }, [keyword]);
+  return <h1>Hello</h1>;
+}
 
-  useEffect(() => {
-    console.log("I run when 'counter' changes"); // counter의 변화가 있을 때만 실행
-  }, [counter]);
-  
-  useEffect(() => {
-    console.log("I run when 'keyword' or 'counter' changes"); // keyword 또는 counter의 변화가 있을 때만 실행
-  }, [keyword, counter]);
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
 
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text" 
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click Me!</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
