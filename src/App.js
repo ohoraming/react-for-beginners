f558a41efe9ddf2bc490f0967fa693c0b056809c
minji <ohora.ming@gmail.com>
2024-04-12 13:35:53 +0900

    coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,35 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]); // 1. 여러 toDo를 받는 array
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") { // toDo가 비어있으면 return
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]); // input을 통해 입력받은 toDo와 빈 array의 element가 더해짐
-    setToDo(""); // input 초기화
-  };
-  console.log(toDos);
-  // console.log(toDos.map((item, index) => (<li key={index}>{item}</li>)));
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []); // useEffect 빈 배열(아무것도 주시하지 않는 상태)은 최초 한 번만 작동함
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1> {/*javascript code 추가*/}
-      <form onSubmit={onSubmit}> {/* Form */}
-        <input 
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (<li key={index}>{item}</li>))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              {coin.name}({coin.symbol}): ${coin.quotes.USD.price}
+            </li>
+          ))}
+        </ul>
+      )
+      }
     </div>
   );
 }
