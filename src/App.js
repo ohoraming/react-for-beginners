ae31a88693fd30340614c741f0522837da0dbc28
minji <ohora.ming@gmail.com>
2024-04-16 09:00:36 +0900

    coin converter

--- src/App.js
+++ src/App.js
@@ -11,21 +11,43 @@ function App() {
       setLoading(false);
     });
   }, []); // useEffect 빈 배열(아무것도 주시하지 않는 상태)은 최초 한 번만 작동함
+  const [myMoney, setMyMoney] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const onChange = (event) => {
+    setMyMoney(event.target.value);
+    console.log(event.target.value);
+  }
+  const onChangeSelect = (event) => {
+    setAmount(event.target.value);
+    console.log(event.target.value);
+  }
+  const usdToCoin = () => {
+    return amount > 0 ? (myMoney / amount) : 0;
+  }
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <ul>
-          {coins.map((coin) => (
-            <li key={coin.id}>
-              {coin.name}({coin.symbol}): ${coin.quotes.USD.price}
-            </li>
-          ))}
-        </ul>
-      )
-      }
+      <h1>coin converter</h1>
+      <span>USD → </span>
+      <select onChange={onChangeSelect}>
+        <option value="0">선택</option>
+        {coins.map((coin) => (
+          <option 
+            key={coin.id}
+            value={coin.quotes.USD.price}
+          >
+            {coin.name}({coin.symbol})
+          </option>
+        ))}
+      </select>
+      <div>
+        <input 
+          type="number"
+          onChange={onChange}
+          value={myMoney}
+          placeholder="USD 입력"
+        /> USD =
+        <span>{usdToCoin()}</span>
+      </div>
     </div>
   );
 }
